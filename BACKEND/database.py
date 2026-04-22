5e01b95c418ea974d59a6af17ed60877197e27c8
vikylyablin <mollyallright@gmail.com>
2026-04-22 13:42:57 +0300

    feat: setup environment variables

--- BACKEND/database.py
+++ BACKEND/database.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, inspect, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./default.db")
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 DATABASE_URL = "sqlite:///./contacts.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
